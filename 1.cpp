264d052944dc4b2e28671ec6b346f09162144211
madao <83685076@qq.com>
2017-10-18 23:39:53 +0800

    shuijiao

--- 1.cpp
+++ 1.cpp
@@ -6,5 +6,6 @@ int main(){
    //git remote add origin git@server-name:path/repo-name.git
    //git remote add origin git@github.com:Kizuki233/learngit.git
    //git push -u origin master
+   //git clone git@github.com:Kizuki233/Sort.git
    return 0;
 }
