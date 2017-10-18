d47839111b5ebb5e4c7e5a6fd144572a12d5bfb4
madao <83685076@qq.com>
2017-10-18 23:33:24 +0800

    keyile

--- 1.cpp
+++ 1.cpp
@@ -6,6 +6,5 @@ int main(){
    //git remote add origin git@server-name:path/repo-name.git
    //git remote add origin git@github.com:Kizuki233/learngit.git
    //git push -u origin master
-   ???
    return 0;
 }
