22488114a27c9f5ef5d363479bf9fcf128a26057
madao <83685076@qq.com>
2017-10-19 19:19:08 +0800

    ok

--- 1.cpp
+++ 1.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int main(){
    cout<<"hello world"<<endl;
-   //git remote add origin git@server-name:path/repo-name.git
+   //git remote add origin git@server-name:path/repo-name.git  远程库的声明
    //git remote add origin git@github.com:Kizuki233/learngit.git
    //git push -u origin master
    //git clone git@github.com:Kizuki233/Sort.git
    //git clone git@github.com:Kizuki233/learngit.git
 
    //git remote -v 远程库
-   //git push origin branch-name git pull
-   //git reset --hard HEAD^
+   //git push origin branch-name       git pull
+   //git reset --hard HEAD^ 会退版本
    //git reflog 版本信息
+   //git add-> commit -m
    return 0;
 }
