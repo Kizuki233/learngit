a8d7300c8e408a1a09934aae946fe43c5e329979
madao <83685076@qq.com>
2017-10-20 11:59:44 +0800

    10-20

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> twoSum(vector<int> &numbers, int target)
+{
+    //Key is the number and value is its index in the vector.
+	unordered_map<int, int> hash;
+	vector<int> result;
+	for (int i = 0; i < numbers.size(); i++) {
+		int numberToFind = target - numbers[i];
+
+            //if numberToFind is found in map, return them
+		if (hash.find(numberToFind) != hash.end()) {
+                    //+1 because indices are NOT zero based
+			result.push_back(hash[numberToFind] + 1);
+			result.push_back(i + 1);			
+			// return result;
+		}
+
+            //number was not found. Put it in the map.
+		hash[numbers[i]] = i;
+	}
+	return result;
+}
+
 int main(){
    cout<<"hello world"<<endl;
    //git remote add origin git@server-name:path/repo-name.git  远程库的声明
@@ -14,5 +36,15 @@ int main(){
    //git reset --hard HEAD^ 会退版本
    //git reflog 版本信息
    //git add-> commit -m
+   int a=1,b=2;
+   a^=b;
+   b^=a;
+   a^=b;
+   cout<<a<<' '<<b<<endl;
+   int arr[10]={1,9,2,8,3,7,4,6,5,5};
+   vector<int> v(arr,arr+10);
+   vector<int> ans=twoSum(v,10);
+   cout<<ans.size()<<endl;
    return 0;
 }
+
